9a61d394f96b0d8a3a149fbf7f454d7ca1c68c6e
takumitesla <takumitesla@gmail.com>
2025-05-09 12:32:26 +0700

    Update autoconfig.sh

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 clear
+pkg install figlet -y
+
+figlet AUTO_CONFIG
+echo "by takumi tesla"
+
+if [ -x /system/xbin/su ] || [ -x /system/bin/su ] || [ -x /sbin/su ] || [ -x /system/su ]; then
+    echo "Perangkat sudah di-root!"
+else
+    echo "are you rooted?"
+fi
