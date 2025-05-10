6160720d210c1bc04a187b04ac8eabb5163486b4
takumitesla <takumitesla@gmail.com>
2025-05-10 16:42:44 +0700

    Update autoconfig.sh

--- autoconfig.sh
+++ autoconfig.sh
@@ -22,3 +22,14 @@ if [ -x /system/xbin/su ] || [ -x /system/bin/su ] || [ -x /sbin/su ] || [ -x /s
 else
     echo "are you rooted?"
 fi
+
+[9/5 22.52] Dr. Maxwell: /data/adb/service.d/autotermux.sh
+[9/5 22.59] Dr. Maxwell: #!/system/bin/sh
+sleep 55
+am start -n com.termux/.app.TermuxActivity
+exit 0
+[9/5 22.59] Dr. Maxwell: /system/build.prop
+[9/5 23.00] Dr. Maxwell: service.adb.tcp.port=5555
+[9/5 23.00] Dr. Maxwell: /system/etc/init/autoboot.rc
+[9/5 23.01] Dr. Maxwell: on charger
+setprop sys.powerctl reboot
